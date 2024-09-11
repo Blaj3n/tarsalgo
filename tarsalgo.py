2a7d3fb5c0a50c7210cdff2192cb33ada35dbf2d
Blaj3n <benceulrich@gmail.com>
2024-09-11 13:47:03 +0200

    3. feladat - ajtón áthaladás txt fájl

--- tarsalgo.py
+++ tarsalgo.py
@@ -26,3 +26,23 @@ for i in range(len(ajto) - 1, -1, -1):  # len(ajtó) - 1, tehát esetünkben 515
 
 
 # 9 5 9 ki
+
+# print("3. feladat")
+
+szemelyek = []
+for szemely in ajto:
+    if szemely[2] not in szemelyek:
+        szemelyek.append(szemely[2])
+print(sorted(szemelyek))    # lista rendezés növekvő sorrendbe (asc)
+
+with open("athaladas.txt", "w", encoding="utf-8") as fajl:
+    for egyelem in sorted(szemelyek):
+        szamlalo = 0
+        for be_ki in ajto:
+            if be_ki[2] == egyelem:
+                szamlalo += 1
+        fajl.write(f"{egyelem} {szamlalo}\n")
+
+# for valtozo in range(5): # [1, 5[ # range(kezdő, meddig menjen -1, hányasával), range(meddig menjen 0-tól - 1) range(5) = [0, 1, 2, 3, 4], range(2, 7) = [2, 3, 4, 5, 6], range(1, 5, 2) = [1, 3]
+
+# HF: 4. feladat lista *
